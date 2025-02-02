4c51628b4535e8f5ec10d4d27c6e41f475543b37
Momin Khan <foolmoron@gmail.com>
2025-02-02 14:36:20 -0500

    gem shading and wobble

--- Gems/gem.gd
+++ Gems/gem.gd
@@ -12,6 +12,12 @@ enum GEM_TYPE {
 
 @onready var mesh := $mesh as MeshInstance3D
 
+@onready var speed := Vector2(randf_range(2.1, 2.6), randf_range(2.1, 2.6))
+@onready var t_x := randf_range(0.0, 2.0)
+@onready var t_z := randf_range(0.0, 2.0)
+@onready var base_x := mesh.rotation_degrees.x
+@onready var base_z := mesh.rotation_degrees.z
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -19,4 +25,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	t_x += delta 
+	t_z += delta
+	mesh.rotation_degrees.x = base_x + sin(t_x * speed.x) * 25
+	mesh.rotation_degrees.z = base_z + sin(t_z * speed.y) * 12
 	pass
